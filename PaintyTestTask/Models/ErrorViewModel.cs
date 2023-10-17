69ff05da4891b9cdf055b3649218590425af1693
timiking5 <tima310103@gmail.com>
2023-10-17 17:46:04 +0300

    Добавьте файлы проекта.

---
+++ PaintyTestTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaintyTestTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
